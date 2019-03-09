61ae269dba6afba6157b8c2aab51e8cdcd1db961
stanstan7 <46615900+stanstan7@users.noreply.github.com>
2019-03-09 12:19:10 +0900

    d

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ namespace soroban {
             led.unplot(0, y)
         }
         if (n < 0) {
-            led.plot(0, ３)
+            led.plot(0, 2)
             n *= -1
         }
         for (let x = 4; x > 0; x--) {
